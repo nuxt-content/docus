d003040d79554303f4f0f68df95dce2593296b2e
Sébastien Chopin <atinux@gmail.com>
2025-09-15 23:57:15 +0200

    feat: support custom layouts

---
+++ layer/app/layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { locale, isEnabled } = useDocusI18n()
+const route = useRoute()
+const isLanding = computed(() => isEnabled.value ? route.path === `/${locale.value}` : route.path === '/')
+</script>
+
+<template>
+  <AppHeader />
+  <UMain>
+    <slot v-if="isLanding" />
+    <UContainer v-else>
+      <UPage>
+        <template #left>
+          <UPageAside>
+            <DocsAsideLeftTop />
+
+            <DocsAsideLeftBody />
+          </UPageAside>
+        </template>
+
+        <slot />
+      </UPage>
+    </UContainer>
+  </UMain>
+  <AppFooter />
+</template>
