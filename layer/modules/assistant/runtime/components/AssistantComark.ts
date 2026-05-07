f842a636c3efc82b4c02ca5f99ad72516e998b63
Hugo Richard <hugo.richard@vercel.com>
2026-05-07 10:10:48 +0100

    fix typecheck

--- layer/modules/assistant/runtime/components/AssistantComark.ts
+++ layer/modules/assistant/runtime/components/AssistantComark.ts
@@ -1,6 +1,5 @@
 import highlight from '@comark/nuxt/plugins/highlight'
 
-// @ts-expect-error - defineComarkComponent is auto-imported by @comark/nuxt
 export default defineComarkComponent({
   name: 'AssistantComark',
   plugins: [
