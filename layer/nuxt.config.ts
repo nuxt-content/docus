73604eb62158c65cd9be492504d787f7d85ae164
Baptiste Leproux <baptiste.leproux@vercel.com>
2026-05-28 19:57:54 +0200

    up deps

--- layer/nuxt.config.ts
+++ layer/nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
         dir: join(nuxt.options.srcDir, 'assets/icons'),
       })
     },
+    '@comark/nuxt',
     '@nuxt/ui',
     '@nuxt/content',
     '@nuxt/image',
